e6acc58f910169705d723af1ea2b1c53567ae5f1
Robertas Liekis <robertasliekis@gmail.com>
2021-02-28 17:32:00 +0200

    initial commit

--- src/reducers/setActiveUser.js
+++ src/reducers/setActiveUser.js
@@ -6,7 +6,8 @@ const setActiveUser = (state = initialState, action) => {
   switch (action.type) {
     case "SET_ACTIVE_USER":
       let data = action.payload;
-      if (state.activeUser !== "") {
+      if (state.activeUser !== "" && data !== "") {
+        console.log("gera");
         data.id = state.activeUser.id;
         data.friendsId = state.activeUser.friendsId;
       }
